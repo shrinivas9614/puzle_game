18544d2c19f86f0b2b4c1c5ba8cfc0ff4486915f
shrinivas9614 <84082921+shrinivas9614@users.noreply.github.com>
2023-07-07 11:34:39 +0530

    Update Match.js

--- src/componants/match/Match.js
+++ src/componants/match/Match.js
@@ -1,42 +1,57 @@
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Image, Row, Col, Container } from 'react-bootstrap';
-import Matching from  './matchimg.json'
-export default function Match() {
-  return (<>
-    <Container>
-    
-      {
-         Matching.qimages.map((imagez,index)=>{
+import { Image, Row, Col, Container } from "react-bootstrap";
+import Matching from "./matchimg.json";
 
-            return(
-            <div  >
+function shuffleArray(array) {
+  // Create a copy of the original array
+  const shuffledArray = [...array];
+  
+  // Shuffle the array using Fisher-Yates algorithm
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+  
+  return shuffledArray;
+}
 
-            <Row 
-            id={index} 
-            style={{ padding: '15px', }} 
-            > 
-            <Col
-             md={1} 
-            >
-                <Image 
-                src={imagez.image} 
-                className="square bg-none rounded" 
-                style={{width: '150px',
-                border: '5px solid blue',
-                padding: '20px'
+
+function MatchThePairsQuestionForm() {
+
+
+  return (
+    <Container  >
+      {Matching.qimages.map((imagez, index) => (
+        <div className="display-flex "> 
+          <Row id={index} style={{ padding: "11px" }}>
+            <Col md={4}>
+              <Image
+                src={imagez.image}
+                className="square bg-none rounded"
+                style={{
+                  width: "150px",
+                  border: "5px solid blue",
+                  padding: "20px",
                 }}
-                />
+              />
             </Col>
-            </Row>
-            </div>
-            )
-
-        })}  
+            <Col md={4} md-offset={1}>
+              <Image
+                src={imagez.image}
+                className="square bg-none rounded"
+                style={{
+                  width: "150px",
+                  border: "5px solid blue",
+                  padding: "20px",
+                }}
+              />
+            </Col>
+          </Row>
+        </div>
+      ))}
     </Container>
-    <Container>
-        
-    </Container>
-  </>
   );
 }
 
+export default MatchThePairsQuestionForm;
